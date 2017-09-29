439bc46fff5ced7b39993c54ee6e07ada11acf42
ItsJonQ <itsjonq@gmail.com>
2017-09-29 17:04:36 +0200

    Add istanbul ignore to log helper methods
    These methods are simple wrappers for console log/error/warn. These exist to
    keep the test logs clean.

--- src/utils/log.js
+++ src/utils/log.js
@@ -6,23 +6,27 @@
  *
  * @returns function
  */
-const logWrapper = fn => message => {
+
+const logWrapper = fn => /* istanbul ignore next */ message => {
   /* istanbul ignore next */
   if (process.env.NODE_ENV !== 'test') {
     return fn(message)
   }
 }
 
+/* istanbul ignore next */
 export const log = message => {
   /* istanbul ignore next */
   return logWrapper(console.log, message)
 }
 
+/* istanbul ignore next */
 export const warn = message => {
   /* istanbul ignore next */
   return logWrapper(console.warn, message)
 }
 
+/* istanbul ignore next */
 export const error = message => {
   /* istanbul ignore next */
   return logWrapper(console.error, message)
